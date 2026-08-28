2f9729c7f0fa1d29e3a07a7d58e6b874c20ec7b7
sameeny1985 <ameeny1said@gmail.com>
2026-08-28 05:38:20 +0200

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart_view, name='cart'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:item_id>/', views.update_cart, name='update_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('order/success/<int:order_id>/', views.order_success, name='order_success'),
+    path('my-orders/', views.my_orders, name='my_orders'),
+]
